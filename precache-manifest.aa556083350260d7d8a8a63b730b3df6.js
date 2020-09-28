e853d282c60cf92a8da44dde33ae5db0b8b17968
Anthony <anthgalea1@gmail.com>
2020-09-28 15:42:53 -0400

    Updates

---
+++ precache-manifest.aa556083350260d7d8a8a63b730b3df6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c4841054bdb1446f11ec28a917ee561",
+    "url": "/anthonyGaleaProjectFive/index.html"
+  },
+  {
+    "revision": "4fe21cb1e59042aec3b6",
+    "url": "/anthonyGaleaProjectFive/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d6c1e91bb950d986c859",
+    "url": "/anthonyGaleaProjectFive/static/js/2.5db2b704.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/anthonyGaleaProjectFive/static/js/2.5db2b704.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fe21cb1e59042aec3b6",
+    "url": "/anthonyGaleaProjectFive/static/js/main.473a6fcd.chunk.js"
+  },
+  {
+    "revision": "efa56695945ce417b52b",
+    "url": "/anthonyGaleaProjectFive/static/js/runtime-main.b85a5b37.js"
+  }
+]);
