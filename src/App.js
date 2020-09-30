4acdc742d53322594465e9eafe1e44c6613dc4ea
Anthony <anthgalea1@gmail.com>
2020-09-30 18:12:21 -0400

    final

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import firebase from './firebase';
 import './App.css';
+import Footer from './Footer.js';
 
 class App extends Component {
   constructor() {
@@ -16,76 +17,68 @@ class App extends Component {
   componentDidMount() {
     // create a Firebase reference and store it in dbRef variable
     const dbRef =  firebase.database().ref(); 
-
-    //listen to the value change and use `response` as the db value
+  
     dbRef.on('value',(response) => {
-      console.log(response.val());
 
       const freshState = [];
       const data = response.val();
 
 
       for(const key in data) {
-        console.log(key, data[key]); //journal property(journal1), data.journal1 journal value (journal entry)
-        //push journal entry inside of a temporary array
+      
 
-        //push data as an object with key property
-        freshState.push({
-          key: key,
-          journalEntry: data[key]
-        });
-      }
+      //data in object with a key 
+      freshState.push({
+        key: key,
+        journalEntry: data[key]
+      });
+    }
 
-      //update our React state for journals
+      //update journals with setState
       this.setState({
         journals: freshState
       });
     });
   }
 
-  //get user input and update the userInput state
+  //update userInput with setState
   handleChange = (event) => {
     this.setState({
       userInput: event.target.value
     });
   }
 
-
-  // step 1: prevent webpage from refreshing
-  // step 2: receive input from user and store it in Firebase
-  // step 3: reset input area
+  //prevent webpage from refreshing
   handleClick = (event) => {
-    event.preventDefault(); //step 1
+    event.preventDefault();
 
     //access to Firebase
     const dbRef = firebase.database().ref(); 
 
-    //add new journal to Firebase
-    dbRef.push(this.state.userInput)//step 2
+    //add new journal to Firebase realtime database
+    dbRef.push(this.state.userInput)
 
     //reset input area
-    this.setState({     //step 3
+    this.setState({ 
       userInput: ''
     });
   }
 
-  // get the specific journal key to remove 
-  // delete a specific journal entry from Firebase
+  // to delete a specific journal entry from Firebase 
   handleRemove = (journalKey) => {
-    console.log(journalKey);
 
-    // open portal to Firebase
+    // access to Firebase
     const dbRef = firebase.database().ref();
 
-    //delete the journal based on journalKey
+    //delete the journal based on the journalKey
     dbRef.child(journalKey).remove();
   }
 
+  // use render to display code on screen
   render() {
     return (
       <div className="App">
         <h1>Personal Journal</h1>
-
 
       {/*form for users to write a journal and store the input in Firebase realtime database */}
       <form action="submit" className="wrapper">
@@ -109,10 +102,7 @@ class App extends Component {
           })
         }
         </ul>
-
-        <footer>
-          <p className="footerParagraph">Created by Anthony Galea at Juno College</p>
-        </footer>
+        <Footer />
       </div>
     )
   }
